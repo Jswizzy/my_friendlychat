ed9f6e47a8793bf1475fd4cc87cf7457e1a61c1d
Jswizzy <smithjustinn@hotmail.com>
2017-08-12 00:02:58 -0400

    finish app

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(new FriendlychatApp());
 }
 
-class FriendlychatApp extends StatelessWidget {
+const String _name = "Justin";
+
+final ThemeData kDefaultTheme = new ThemeData(
+  primarySwatch: Colors.purple,
+  accentColor: Colors.orangeAccent[400],
+);
+
+final ThemeData kIOSTHEME = new ThemeData(
+  primarySwatch: Colors.orange,
+  primaryColor: Colors.grey[100],
+  primaryColorBrightness: Brightness.light,
+);
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final animationController;
+
+  ChatMessage({this.text, this.animationController});
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: "Friendlychat",
-      home: new ChatScreen(),
+    return new SizeTransition(
+      sizeFactor: new CurvedAnimation(
+          parent: animationController, curve: Curves.easeOut),
+      axisAlignment: 0.0,
+      child: new Container(
+        margin: const EdgeInsets.symmetric(vertical: 10.0),
+        child: new Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              margin: const EdgeInsets.only(right: 16.0),
+              child: new CircleAvatar(child: new Text(_name[0])),
+            ),
+            new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(_name, style: Theme.of(context).textTheme.subhead),
+                new Container(
+                  margin: const EdgeInsets.only(top: 5.0),
+                  child: new Text(text),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -19,9 +62,52 @@ class ChatScreen extends StatefulWidget {
   State createState() => new ChatScreenState();
 }
 
-class ChatScreenState extends State<ChatScreen> {
+class ChatScreenState extends State<ChatScreen> with TickerProviderStateMixin {
   final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
+  bool _isComposing = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Friendlychat"),
+        elevation: Theme.of(context).platform == TargetPlatform.iOS ? 0.0 : 4.0,
+      ),
+      body: new Container(
+          child: new Column(
+            children: <Widget>[
+              new Flexible(
+                child: new ListView.builder(
+                  padding: new EdgeInsets.all(8.0),
+                  reverse: true,
+                  itemBuilder: (_, int index) => _messages[index],
+                  itemCount: _messages.length,
+                ),
+              ),
+              new Divider(height: 1.0),
+              new Container(
+                decoration: new BoxDecoration(
+                    color: Theme.of(context).cardColor),
+                child: _buildTextComposer(),
+              ),
+            ],
+          ),
+          decoration: Theme.of(context).platform == TargetPlatform.iOS
+              ? new BoxDecoration(
+              border:
+              new Border(top: new BorderSide(color: Colors.grey[200])))
+              : null),
+    );
+  }
+
+  @override
+  void dispose() {
+    for (ChatMessage message in _messages) {
+      message.animationController.dispose();
+    }
+    super.dispose();
+  }
 
   Widget _buildTextComposer() {
     return new IconTheme(
@@ -33,6 +119,11 @@ class ChatScreenState extends State<ChatScreen> {
                 new Flexible(
                   child: new TextField(
                     controller: _textController,
+                    onChanged: (String text) {
+                      setState(() {
+                        _isComposing = text.length > 0;
+                      });
+                    },
                     onSubmitted: _handleSubmitted,
                     decoration: new InputDecoration.collapsed(
                         hintText: "Send a message"),
@@ -40,82 +131,49 @@ class ChatScreenState extends State<ChatScreen> {
                 ),
                 new Container(
                   margin: new EdgeInsets.symmetric(horizontal: 4.0),
-                  child: new IconButton(
-                      icon: new Icon(Icons.send),
-                      onPressed: () => _handleSubmitted(_textController.text)),
+                  child: Theme.of(context).platform == TargetPlatform.iOS
+                      ? new CupertinoButton(
+                          child: new Text("Send"),
+                          onPressed: _isComposing
+                              ? () => _handleSubmitted(_textController.text)
+                              : null)
+                      : new IconButton(
+                          icon: new Icon(Icons.send),
+                          onPressed: _isComposing
+                              ? () => _handleSubmitted(_textController.text)
+                              : null),
                 ),
               ],
             )));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Friendlychat"),
-      ),
-      body: new Column(
-        children: <Widget>[
-          new Flexible(child: new ListView.builder(
-            padding: new EdgeInsets.all(8.0),
-            reverse: true,
-            itemBuilder: (_, int index) => _messages[index],
-            itemCount: _messages.length,
-          )),
-          new Divider(height: 1.0),
-          new Container(
-            decoration: new BoxDecoration(
-              color: Theme.of(context).cardColor),
-            child: _buildTextComposer(),
-          )
-        ],
-      )
-    );
-  }
-
   void _handleSubmitted(String text) {
     _textController.clear();
+    setState(() {
+      _isComposing = false;
+    });
 
     ChatMessage message = new ChatMessage(
       text: text,
+      animationController: new AnimationController(
+          duration: new Duration(milliseconds: 700), vsync: this),
     );
-
     setState(() {
       _messages.insert(0, message);
     });
+    message.animationController.forward();
   }
 }
 
-const String _name = "Justin";
-
-class ChatMessage extends StatelessWidget {
-  final String text;
-
-  ChatMessage({this.text});
-
+class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: new Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          new Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: new CircleAvatar(child: new Text(_name[0])),
-          ),
-          new Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              new Text(_name, style: Theme.of(context).textTheme.subhead),
-              new Container(
-                margin: const EdgeInsets.only(top: 5.0),
-                child: new Text(text),
-              )
-            ],
-          ),
-        ],
-      ),
+    return new MaterialApp(
+      title: "Friendlychat",
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+          ? kIOSTHEME
+          : kDefaultTheme,
+      home: new ChatScreen(),
     );
   }
 }
